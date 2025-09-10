7693811564b84aee800934b42f834e8114a1d34a
Mahin <mashrafi.devs@gmail.com>
2025-09-10 11:51:53 +0600

    File Upload Preview

--- app.js
+++ app.js
@@ -1,82 +1,65 @@
-const user_name = document.querySelector('#name');
-const user_email = document.querySelector('#email');
-const user_mobile = document.querySelector('#phone');
-const user_message = document.querySelector('#message');
-const user_checkbox = document.querySelectorAll('.checkbox input');
-const sendBtn = document.querySelector('#send');
+// get the values inside variable
+const fileInput = document.querySelector('#fileInput')
+const preview = document.querySelector('.preview')
+const placeholder = document.querySelector('#placeholder')
 
-// get local storage data record
-let user = JSON.parse(localStorage.getItem('Name')) || [];
-
-// storage pattern
-function Pattern(username, email, mobile, message, choice) {
-    return {
-        user: username,
-        email: email,
-        mobile: mobile,
-        message: message,
-        interests: choice
-    }
-}
-
-// store data to local 
-function Store(username, object) {
-    // checkpoint
-    let currentCheck = localStorage.getItem('Name');
-    let userLower = username.toLowerCase();
+// main change function
+fileInput.addEventListener('change', () => {
+    // store file lists inside variable
+    const files = fileInput.files;
     
-    if (
-        currentCheck !== null &&
-        currentCheck.includes(userLower)
-    ) {
-        alert('Already added into storage using this name. Please add another name to save data.');
-        return;
+    // validation for placeholder
+    if (files.length > 0) {
+        placeholder.style.display = 'none';
     }
 
-    // push record to usesr variable
-    user.push(userLower);
+    // loop to see through each file
+    for (const file of files) {
+        
+        // creating image url
+        let imageLink = URL.createObjectURL(file);
+        
+        // creating elements with attributes
+        let image = document.createElement('div');
+        image.setAttribute('class', 'image');
+        let img = document.createElement('img');
+        img.src = imageLink;
+        img.setAttribute('draggable', false);
+        let icon = document.createElement('i');
+        icon.setAttribute('class', 'fa-solid fa-times');
+        let size = document.createElement('p');
+        
+        // set image size value as null initially 
+        let sizeValue = null;
+        
+        // calculate the size of image
+        if ((file.size / 1024) <= 1024) {
+            sizeValue = Math.floor((file.size / 1024)) + ' KB'
+        } 
+        else if ((file.size / 1024) > 1024) {
+            sizeValue = Math.ceil((file.size / 1024) / 1024) + ' MB'
+        } 
 
-    // store data 
-    localStorage.setItem('Name', JSON.stringify(user));
-    localStorage.setItem(userLower, JSON.stringify(object));
-}
-
-// Main function 
-function Data() {
-    // get values
-    const username = user_name.value.trim();
-    const email = user_email.value.trim();
-    const mobile = user_mobile.value.trim();
-    const message = user_message.value.trim();
-
-    // validation
-    if (username === '') {
-        alert('Please add details to save in storage.');
-        return;
-    }
-    
-    // multiple choices
-    let multipleChoice = [];
-    user_checkbox.forEach(item => {
-        if (item.checked) {
-            multipleChoice.push(item.value);
+        // add size inside the targeted variable
+        size.textContent = sizeValue;
+        
+        // append all the elements inside HTML
+        image.append(img, icon, size);
+        preview.append(image);
+        
+        // memory free up method
+        img.onload = () => {
+            URL.revokeObjectURL(imageLink);
         }
-    });
+        
+        // cross icon close function
+        icon.addEventListener('click', (e) => {
+            e.currentTarget.parentElement.remove()
 
-
-    // store data for pattern
-    let object = Pattern(username, email, mobile, message, multipleChoice);
-
-    // main storage
-    Store(username, object)
-
-    console.log(multipleChoice)
-}
-
-// run function on click
-sendBtn.addEventListener('click', (e) => {
-    // prevent page from reload
-    e.preventDefault();
-    // call the main function
-    Data()
+            // if there's no image div then put back the placeholder
+            if (preview.children.length === 1) {
+                placeholder.style.display = 'block';
+            }
+        })
+    }
 })
